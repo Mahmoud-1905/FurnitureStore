5b52b629258b5ac408867a73e433698f48d720d0
Mahmoud <mahmoud.m.alhendy@gmail.com>
2026-04-04 16:29:47 +0300

    add code to site.cs add code to Home controller add Products controller add Products Models add code to views - Home - Index.cshtml add Views - Products - Index add code to Views - Shared - _Layout.cshtml

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using FurnitureStore.Models;
+using FurnitureStore.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FurnitureStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>()
+            {
+                new Product { Id = 1, Name = "Product1", Price = 500, ImageUrl = "~/images/3.jpg" },
+                new Product { Id = 2, Name = "Product2", Price = 200, ImageUrl = "~/images/7.jpg" }
+            };
+
+            return View(products);
+        }
+    }
+}
