26f8ca8d4909a50f555c5cf554b76b47b7d3ea45
Raghad Akila <raghadakila@gmail.com>
2026-04-05 22:14:40 +0300

    Two new products were added to the productsController, a new module named LoginViewModel was added, the .index in view/Home page was modified, the .index in view/products page was modified, and the layout.cshtml file was modified and modified site.css file.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -12,8 +12,10 @@ namespace FurnitureStore.Controllers
         {
             var products = new List<Product>()
             {
-                new Product { Id = 1, Name = "Product1", Price = 500, ImageUrl = "~/images/3.jpg" },
-                new Product { Id = 2, Name = "Product2", Price = 200, ImageUrl = "~/images/7.jpg" }
+                new Product { Id = 1, Name = "Modern corner sofa, Allora off-white", Price = 500, ImageUrl = "~/images/3.jpg" },
+                new Product { Id = 2, Name = "5-piece serving table set - luxurious and professional design", Price = 200, ImageUrl = "~/images/7.jpg" },
+                new Product { Id = 3, Name = "A distinctive swing with an elegant circular design", Price = 150, ImageUrl = "~/images/4.jpg" },
+                new Product { Id = 4, Name = "Modern and elegant dining table with 8 chairs", Price = 300, ImageUrl = "~/images/5.jpg" }
             };
 
             return View(products);
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FurnitureStore.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
