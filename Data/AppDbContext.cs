81e06d4f7ee8d036ec837c3df963114f382f1bf3
Mahmoud <mahmoud.m.alhendy@gmail.com>
2026-04-09 12:31:14 +0300

    add database

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,15 +2,11 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using FurnitureStore.Models;
-
-
-
-
 namespace FurnitureStore.Data
 {
     public class AppDbContext : IdentityDbContext<Users>
     {
-        public AppDbContext(DbContextOptions options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
     }
